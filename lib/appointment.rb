d681bb718c83ad9837cd9d94c6777082204529ed
Vanessa Toro <vanetoro84@gmail.com>
2018-04-03 22:14:04 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
     
     def initialize(title, doctor)
       @title = title
-      doctor.add_appointment(title) = self
+      doctor.add_appointment = self
     end
     
     # def add_appointment
