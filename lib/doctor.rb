f9b1366c1ea5d2dd546f4e77b4096efc08f5a57a
Vanessa Toro <vanetoro84@gmail.com>
2018-04-03 22:15:46 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -7,9 +7,8 @@ class Doctor
       @appointments = []
     end  
     
-    def add_appointment
-      binding.pry
-      @appointments << self
+    def add_appointment(appointment)
+      @appointments << appointment
       appointment.doctor = self
     end
     
